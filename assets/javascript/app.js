744ad06c3fba7c00e1849b6f00e52052b2f8cd75
Darrin Davis <dezz001@yahoo.com>
2018-02-02 13:35:52 -0500

    Updated Files and Folders - Dezz

---
+++ assets/javascript/app.js
@@ -0,0 +1,69 @@
+
+
+// Initialize Firebase
+var config = {
+	apiKey: 
+	authDomain: 
+	databaseURL: 
+	projectId: 
+	storageBucket: 
+	messagingSenderId: 
+};
+
+firebase.initializeApp(config);
+
+// Creating Database Reference
+var databaseReference = firebase.database();
+
+
+
+// Global Input Variables	
+
+
+
+// Global Variables - For Posting
+
+
+
+// Submits Form Information
+$("#submit-button").on("click", function() {
+
+	// Form Information - Examples
+		// trainName = $("#trainName-input").val().trim();
+		// destination = $("#destination-input").val().trim();
+		// firstTrainTime = $("#firstTrainTime-input").val().trim();
+		// frequency = $("#frequency-input").val().trim();
+
+
+	// Database conncted variables
+	databaseReference.ref().set({
+		// Examples
+			// trainName: trainName,
+			// destination: destination,
+			// frequency: frequency,
+			// nextArrivalTime: nextArrivalTime,
+			// minutesUntilTrainArrive: minutesUntilTrainArrive
+	});
+
+});
+
+
+// Firebase Watcher
+databaseReference.ref().on("value", function(snapshot) {
+
+	var remoteData = snapshot.val();
+
+	// Debugging - Examples
+		// console.log(remoteData);
+		// console.log(remoteData.trainName);
+		// console.log(remoteData.destination);
+		// console.log(remoteData.firstTrainTime);
+		// console.log(remoteData.frequency);
+	
+	},
+
+	// Handle Debuggin Errors
+	function (errorObject) {
+		console.log("Errors Handled: " + errorObject.code);
+	}
+);
