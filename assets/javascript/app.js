b8570179c65e7ad3e309709e6e39499964d6490b
Donna England <mrsengland@aol.com>
2018-02-07 18:25:11 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,19 @@
 
 
 // Initialize Firebase
-var config = {
-	apiKey: 
-	authDomain: 
-	databaseURL: 
-	projectId: 
-	storageBucket: 
-	messagingSenderId: 
-};
+// var config = {
+// 	apiKey: 
+// 	authDomain: 
+// 	databaseURL: 
+// 	projectId: 
+// 	storageBucket: 
+// 	messagingSenderId: 
+// };
 
-firebase.initializeApp(config);
+// firebase.initializeApp(config);
 
 // Creating Database Reference
-var databaseReference = firebase.database();
+// var databaseReference = firebase.database();
 
 
 
@@ -26,7 +26,7 @@ var databaseReference = firebase.database();
 
 
 // Submits Form Information
-$("#submit-button").on("click", function() {
+// $("#submit-button").on("click", function() {
 
 	// Form Information - Examples
 		// trainName = $("#trainName-input").val().trim();
@@ -36,22 +36,22 @@ $("#submit-button").on("click", function() {
 
 
 	// Database conncted variables
-	databaseReference.ref().set({
+	// databaseReference.ref().set({
 		// Examples
 			// trainName: trainName,
-			// destination: destination,
-			// frequency: frequency,
-			// nextArrivalTime: nextArrivalTime,
-			// minutesUntilTrainArrive: minutesUntilTrainArrive
-	});
+// 			// destination: destination,
+// 			// frequency: frequency,
+// 			// nextArrivalTime: nextArrivalTime,
+// 			// minutesUntilTrainArrive: minutesUntilTrainArrive
+// 	});
 
-});
+// });
 
 
-// Firebase Watcher
-databaseReference.ref().on("value", function(snapshot) {
+// // Firebase Watcher
+// databaseReference.ref().on("value", function(snapshot) {
 
-	var remoteData = snapshot.val();
+// 	var remoteData = snapshot.val();
 
 	// Debugging - Examples
 		// console.log(remoteData);
@@ -60,10 +60,10 @@ databaseReference.ref().on("value", function(snapshot) {
 		// console.log(remoteData.firstTrainTime);
 		// console.log(remoteData.frequency);
 	
-	},
+// 	},
 
-	// Handle Debuggin Errors
-	function (errorObject) {
-		console.log("Errors Handled: " + errorObject.code);
-	}
-);
+// 	// Handle Debuggin Errors
+// 	function (errorObject) {
+// 		console.log("Errors Handled: " + errorObject.code);
+// 	}
+// );
