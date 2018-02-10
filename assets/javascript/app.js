e1b562eee7f4bf6b9234e7393b79a35fc38925ca
Ethan <erogers999@gmail.com>
2018-02-10 10:50:57 -0500

    Update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,3 +5,9 @@ $("#no-button").on("click", function (){
 $("#yes-button").on("click", function (){
 	window.location.href='index-FoodResults.html'
 });
+
+
+$(document).on('ready', function (){
+	console.log(higestVal.emotion);
+
+})
