9f246f9080b0d408c70737d5ced44b5c5afd92b2
Ethan <erogers999@gmail.com>
2018-02-11 19:09:30 -0500

    Update

--- assets/javascript/emotion-loop.js
+++ assets/javascript/emotion-loop.js
@@ -4,32 +4,32 @@ console.log(mood);
 if (mood == "anger") {
    //Display the anger emoji in the div (#emoji-display) along with the word "ANGRY" in the div (#emotion-text) on the results-index page
    $("#emoji-display").attr('src', 'assets/images/angry-emoji.png');
-   $("#emoji-display-text").text('ANGRY');
+   $("#emotion-display-text").text('ANGRY');
 
 } else if (mood == "disgust") {
    $("#emoji-display").attr('src', 'assets/images/disgust-emoji.png');
-   $("#emoji-display-text").text('DISGUSTED');
+   $("#emotion-display-text").text('DISGUSTED');
 
 } else if (mood == "fear") {
     console.log("Im Scared Emoji");
    $("#emoji-display").attr('src', 'assets/images/fear-emoji.png');
-   $("#emoji-display-text").text('FEARFUL');
+   $("#emotion-display-text").text('FEARFUL');
 
 } else if (mood == "happiness") {
    $("#emoji-display").attr('src', 'assets/images/happy-emoji.png');
-   $("#emoji-display-text").text('HAPPY');
+   $("#emotion-display-text").text('HAPPY');
 
 } else if (mood == "neutral") {
    $("#emoji-display").attr('src', 'assets/images/neutral-emoji.png');
-   $("#emoji-display-text").text('NEUTRAL');
+   $("#emotion-display-text").text('NEUTRAL');
 
 } else if (mood == "sadness") {
    $("#emoji-display").attr('src', 'assets/images/sad-emoji.png');
-   $("#emoji-display-text").text('SAD');
+   $("#emotion-display-text").text('SAD');
 
 } else if (mood == "surprise") {
    $("#emoji-display").attr('src', 'assets/images/surprise-emoji.png');
-   $("#emoji-display-text").text('SURPRISED');
+   $("#emotion-display-text").text('SURPRISED');
 
 } else {
    //code that will execute if all above conditions are false
